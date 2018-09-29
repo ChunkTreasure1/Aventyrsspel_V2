8cbe21eca1d0721036a134eda840e132a8bde206
Ivar Jönsson <ivar.jonsson@shadowgamestudio.net>
2018-09-29 21:54:58 +0200

    First

---
+++ Äventyrspel_v2/FightSystem.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Äventyrspel_v2 {
+    class FightSystem {
+
+        List<Attack> Attacks = new List<Attack>();
+
+        public int PlayerHealth = 100;
+        public bool IsAlive = true;
+
+        public Attack GunShot = new Attack();
+        public Attack ShotgunShot = new Attack();
+        public Attack SniperShot = new Attack();
+
+        //Called when the player should attack an enemy
+        public void Attack(Enemy enemyToAttack) {
+
+            //While the player and the enemy is still alive
+            while (IsAlive && enemyToAttack.IsAlive) {
+
+                //Get the players and the enemys attack
+                Attack attackToUse = GetAttack();
+                Attack enemyAttack = enemyToAttack.GetEnemyAttack();
+
+                //If the players attack speed is less than the enemys attack speed
+                if (attackToUse.AttackSpeed < enemyAttack.AttackSpeed) {
+
+                    //Damage the player
+                    PlayerHealth -= enemyAttack.AttackDamage;
+
+                    //If both the player and the enemy is still alive
+                    if (PlayerHealth > 0 && enemyToAttack.Healh > 0) {
+
+                        //Damage the enemy
+                        enemyToAttack.Healh -= attackToUse.AttackDamage;
+
+                        //Show the enemys and the players health
+                        Console.Clear();
+                        Console.WriteLine("Attack Successful!");
+                        ShowHealth(enemyToAttack);
+
+                        Console.WriteLine("Press ENTER to attack again");
+                        Console.ReadKey();
+
+                    }
+                    //If one of them is not alive
+                    else {
+
+                        //Check who it is that is dead and end the loop
+                        if (PlayerHealth <= 0) {
+                            IsAlive = false;
+                            break;
+                        }
+                        else if (enemyToAttack.Healh <= 0) {
+                            enemyToAttack.IsAlive = false;
+                            break;
+                        }
+
+                    }
+                }
+                //If the players attack speed is greater than the enemys
+                else if (attackToUse.AttackSpeed > enemyAttack.AttackSpeed) {
+
+                    //Damage the enemy
+                    enemyToAttack.Healh -= attackToUse.AttackDamage;
+
+                    //If the player and the enemy is still alive
+                    if (enemyToAttack.Healh > 0 && PlayerHealth > 0) {
+
+                        PlayerHealth -= enemyAttack.AttackDamage;
+
+                        //Show the players and the enemys health
+                        Console.Clear();
+                        Console.WriteLine("Attack Successful!");
+                        ShowHealth(enemyToAttack);
+
+                        Console.WriteLine("Press ENTER to attack again");
+                        Console.ReadKey();
+
+                    }
+                    //If on of them is dead
+                    else {
+
+                        //Check who it is that is dead
+                        if (PlayerHealth <= 0) {
+                            IsAlive = false;
+                            break;
+                        }
+                        else if (enemyToAttack.Healh <= 0) {
+                            enemyToAttack.IsAlive = false;
+                            break;
+                        }
+
+                    }
+
+                }
+                //If the attack speeds are the same
+                else if (attackToUse.AttackSpeed == enemyAttack.AttackSpeed) {
+
+                    //Tell the player that the attacks have cancelled out
+                    Console.Clear();
+                    Console.WriteLine("The attack speed is the same! The attacks have cancelled out!");
+                    ShowHealth(enemyToAttack);
+
+                    Console.WriteLine("Press ENTER to attack again");
+
+
+                }
+
+            }
+
+            //Check if the player is alive, otherwise the player 
+            //died and need to rerun the game
+            if (IsAlive == true) {
+
+                //Tell the player that the enemy died
+                Console.Clear();
+                Console.WriteLine("Enemy died!");
+                Console.WriteLine("Your health: " + PlayerHealth);
+                Console.WriteLine("Press ENTER to continue");
+                Console.ReadKey();
+
+            }
+            else {
+
+                //Tell the player that it died and that it's game over
+                Console.Clear();
+                Console.WriteLine("You died :(");
+                Console.WriteLine("Press ENTER to continue");
+
+                //Shows the game over screen
+                ShowGameOver();
+
+            }
+
+        }
+
+        //Shows the attack list and lets you choose which to use
+        Attack GetAttack() {
+
+            bool hasChosen = false;
+
+            while (!hasChosen) {
+
+                //Clears the console
+                Console.Clear();
+
+                for (int i = 0; i < Attacks.Count; i++) {
+
+                    Console.WriteLine((i + 1) + " - " + Attacks[i].AttackName);
+                }
+                //Get the number to get the attack
+                string select = Console.ReadLine();
+
+                //Return the attack that has been selected with the entered number above
+                if (select == "1") {
+                    return Attacks[0];
+                }
+                else if (select == "2") {
+                    return Attacks[1];
+                }
+                else if (select == "3") {
+                    return Attacks[2];
+                }
+                else if (select == "4") {
+                    return Attacks[3];
+                }
+                else if (select == "5") {
+                    return Attacks[4];
+                }
+                else {
+                    Console.WriteLine("Incorrect input!");
+                }
+
+            }
+
+            return new Attack();
+
+        }
+
+        //Prints out the health of the player and the enemy
+        void ShowHealth(Enemy enemyToAttack) {
+
+            Console.WriteLine("Enemy health: " + enemyToAttack.Healh);
+            Console.WriteLine("Player health: " + PlayerHealth);
+
+        }
+
+        //Shows the game over screen
+        void ShowGameOver() {
+
+            Console.Clear();
+
+            var gameOverScreen = new[] {
+
+                @"----------------------------------------------------------------------",
+                @"-                                                                    -",
+                @"-                                                                    -",
+                @"-                                                                    -",
+                @"-                                                                    -",
+                @"-                                                                    -",
+                @"-                            Game Over                               -",
+                @"-                                                                    -",
+                @"-                                                                    -",
+                @"-                                                                    -",
+                @"-                                                                    -",
+                @"-                      Press ENTER to continue                       -",
+                @"-                                                                    -",
+                @"----------------------------------------------------------------------",
+            };
+
+            //Writes out the game over screen
+            foreach (string line in gameOverScreen) {
+
+                Console.WriteLine(line);
+
+            }
+
+        }
+
+        //Adds an attack to the attacks list
+        public void AddAttack(Attack attack) {
+
+            Attacks.Add(attack);
+
+        }
+    }
+
+    class Attack {
+
+        public string AttackName;
+        public int AttackSpeed;
+        public int AttackDamage;
+
+    }
+
+    class Enemy {
+
+        public string name;
+
+        public bool IsAlive = true;
+        public int Healh;
+        public int MaxEnemyAttacks;
+
+        List<Attack> EnemyAttacks = new List<Attack>();
+
+        //Gets a random attack for the enemy
+        public Attack GetEnemyAttack() {
+
+            //Returns a random attack from the list if enemy attacks
+            Random random = new Random();
+            return EnemyAttacks[random.Next(0, (MaxEnemyAttacks))];
+
+        }
+    }
+
+}
---
+++ Äventyrspel_v2/Program.cs
@@ -0,0 +1,419 @@
+﻿using System;
+using System.Timers;
+using System.Threading;
+
+namespace Äventyrspel_v2 {
+    class Program {
+
+        enum EGameConditions {
+
+            eGC_PlainCity,
+            eGC_Manhattan,
+            eGC_LasVegas
+
+        }
+
+        FightSystem PlayerFightSystem = new FightSystem();
+        System.Timers.Timer SleepTimer;
+
+        EGameConditions ConditionsToUse;
+
+        string PlayerName { get; set; }
+        string HungerStatus = "Full";
+
+        bool IsSleeping = false;
+
+        int RandomEnemyCountMax;
+        int RandomEnemyCountMin;
+        int DaysAlive = 0;
+
+        int GameHours = 0;
+
+        static void Main(string[] args) {
+
+            //Sets the colors of the console
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            //Sets the size of the console
+            Console.WindowWidth = 90;
+            Console.WindowHeight = 20;
+
+            Console.Clear();
+
+            //Create the new program object
+            Program program = new Program();
+
+            //Starts the game
+            program.StartGame();
+        }
+
+        //Method that handels all the start funcitonality
+        void StartGame() {
+
+            //Show the start screen
+            ShowStartScreen();
+            Console.ReadKey();
+
+            Console.Clear();
+            //Bool to check if the player has confirmed it's name
+            bool isConfirmed = false;
+
+            while (!isConfirmed) {
+
+                Console.Write("Please state your name: ");
+                PlayerName = Console.ReadLine();
+
+                Console.Clear();
+
+                //Ask if the name the player entered was correct
+                Console.WriteLine("Is " + PlayerName + " correct?");
+                Console.WriteLine("[Y]/[N]");
+
+                string answer = Console.ReadLine();
+
+                //If the player answered YES
+                if (answer == "Y" || answer == "y") {
+
+                    //Clear the screen and continue
+                    Console.Clear();
+                    Console.WriteLine("Alright great!");
+                    Console.WriteLine("Press ENTER to continue");
+
+                    isConfirmed = true;
+                    Console.ReadLine();
+
+                }
+                //If the player answered NO
+                else if (answer == "N" || answer == "n") {
+
+                    //Clear the screen and continue
+                    Console.Clear();
+                    Console.WriteLine("Aww too bad, guess you'll have to enter it again!");
+                    Console.WriteLine("Press ENTER to continue");
+
+                    Console.ReadKey();
+
+                }
+                else {
+
+                    //Clear the screen and let the player re enter it's name
+                    //as this is assuming that it's a no
+
+                    Console.Clear();
+                    Console.WriteLine("Incorrect input! Assuming no!");
+                    Console.WriteLine("Press ENTER to continue");
+
+                    Console.ReadKey();
+
+                }
+
+            }
+
+            //Let's the player choose it's start location
+            ChooseStartLocation();
+
+            //Set's the game conditions based on what you entered
+            SetGameConditions();
+            SetAttacks();
+
+            Thread t = new Thread(() => {
+
+                System.Timers.Timer GameTimer = null;
+                bool timerSet = false;
+
+                StartGameTimer();
+
+                //Starts the games time timer
+                void StartGameTimer() {
+
+                    //Always run this loop
+                    while (true) {
+
+                        if (timerSet) {
+                            //Start a game timer that lasts for one game hour
+                            GameTimer = SetGameTimer(2500);
+                            timerSet = true;
+                        }
+
+                        //If GameHours is equal to or greater than 24
+                        if (GameHours >= 24) {
+
+                            //Set game hours to 0 and and add one to the days
+                            GameHours = 0;
+                            DaysAlive++;
+
+                        }
+
+                    }
+
+                }
+
+                //Called when the game timer has elapsed
+                void GameTimerElapsed(object sender, ElapsedEventArgs e) {
+
+                    GameHours++;
+
+                    GameTimer.Stop();
+                    GameTimer.Dispose();
+                    timerSet = false;
+
+                }
+
+                //Creates a game timer
+                System.Timers.Timer SetGameTimer(int time) {
+
+                    System.Timers.Timer aTimer = new System.Timers.Timer();
+                    aTimer.Elapsed += new ElapsedEventHandler(GameTimerElapsed);
+                    aTimer.Interval = time;
+                    aTimer.Enabled = true;
+
+                    return aTimer;
+
+                }
+
+            });
+            t.Start();
+
+            //Show main UI
+            ShowMainUI();
+
+        }
+
+        //Shows the start screen. Called at the start of the game
+        void ShowStartScreen() {
+
+            var startScreen = new[] {
+
+                @"----------------------------------------------------------------------",
+                @"-                                                                    -",
+                @"-                                                                    -",
+                @"-                                                                    -",
+                @"-                         Apocalyptic World                          -",
+                @"-                                                                    -",
+                @"-                      A Game by ChunkTreasure                       -",
+                @"-                                                                    -",
+                @"-                                                                    -",
+                @"-                                                                    -",
+                @"-                                                                    -",
+                @"-                      Press ENTER to continue                       -",
+                @"-                                                                    -",
+                @"----------------------------------------------------------------------",
+            };
+
+            foreach (string line in startScreen) {
+                Console.WriteLine(line);
+                System.Threading.Thread.Sleep(5);
+            }
+
+        }
+        
+        //Lets the player choose the it's start location
+        void ChooseStartLocation() {
+
+            //Clear the console
+            Console.Clear();
+
+            //Show the start locations
+            Console.WriteLine("Time to choose a starting location!");
+            Console.WriteLine("1 - Plain City");
+            Console.WriteLine("2 - Manhattan");
+            Console.WriteLine("3 - Las Vegas");
+
+            string location = Console.ReadLine();
+
+            //Bool to check if the player has chosen it's start location
+            bool hasChosen = false;
+
+            //While the player has not chosen a start location
+            while (!hasChosen) {
+
+                if (location == "1") {
+                    //Set the game conditions to Plain City
+                    ConditionsToUse = EGameConditions.eGC_PlainCity;
+                    hasChosen = true;
+                    break;
+                }
+                else if (location == "2") {
+                    //Set the game conditions to Manhattan
+                    ConditionsToUse = EGameConditions.eGC_Manhattan;
+                    hasChosen = true;
+                    break;
+                }
+                else if (location == "3") {
+                    //Set the game conditions to Las Vegas
+                    ConditionsToUse = EGameConditions.eGC_LasVegas;
+                    hasChosen = true;
+                    break;
+                }
+                else {
+                    Console.WriteLine("Incorrect input!");
+                    Console.WriteLine("Press ENTER to continue");
+                    Console.ReadKey();
+
+                    Console.Clear();
+                }
+
+            }
+
+        }
+
+        //Sets the conditions of the game
+        void SetGameConditions() {
+
+            //If the game conditions are set to Las Vegas
+            if (ConditionsToUse == EGameConditions.eGC_LasVegas) {
+
+                //Set the vars
+                RandomEnemyCountMax = 5;
+                RandomEnemyCountMin = 2;
+
+                //Start attacks
+                PlayerFightSystem.AddAttack(PlayerFightSystem.GunShot);
+                PlayerFightSystem.AddAttack(PlayerFightSystem.ShotgunShot);
+
+            }
+            else if (ConditionsToUse == EGameConditions.eGC_Manhattan) {
+
+                //Set the vars
+                RandomEnemyCountMax = 7;
+                RandomEnemyCountMin = 1;
+
+                //Start attacks
+                PlayerFightSystem.AddAttack(PlayerFightSystem.SniperShot);
+                PlayerFightSystem.AddAttack(PlayerFightSystem.ShotgunShot);
+
+            }
+            else if (ConditionsToUse == EGameConditions.eGC_PlainCity) {
+
+                //Set the vars
+                RandomEnemyCountMax = 3;
+                RandomEnemyCountMin = 1;
+
+                //Start attacks
+                PlayerFightSystem.AddAttack(PlayerFightSystem.SniperShot);
+                PlayerFightSystem.AddAttack(PlayerFightSystem.GunShot);
+
+            }
+
+        }
+
+        void ShowMainUI() {
+
+            //Always runs because we should always get back here
+            while (true) {
+
+                Console.Clear();
+
+                var menu = new[] {
+
+                @"Player healh: " + PlayerFightSystem.PlayerHealth + "             " + "Days alive: " + DaysAlive + "             " + "Hunger: " + HungerStatus,
+                @"",
+                @"What would you like to do?",
+                @"1 - Go out and venture",
+                @"2 - Sleep",
+                @"3 - Access inventory",
+                @"4 - Eat"
+
+            };
+
+                //Show the menu with a short delay between the lines to give an effect
+                foreach (string line in menu) {
+                    Console.WriteLine(line);
+                    System.Threading.Thread.Sleep(30);
+                }
+
+                //Get the players choice
+                string playerChoice = Console.ReadLine();
+
+                //If the player choses one
+                if (playerChoice == "1") {
+
+                }
+                //If the player choses two
+                else if (playerChoice == "2") {
+
+                    //Let's the player sleep
+                    StartSleep();
+
+                }
+                //If the player choses three
+                else if (playerChoice == "3") {
+
+                }
+                //If the player choses four
+                else if (playerChoice == "4") {
+
+                }
+                else {
+
+                    Console.WriteLine("Incorrect input!");
+                    Console.WriteLine("Press ENTER to continue");
+
+                    Console.ReadKey();
+                }
+
+            }
+
+        }
+
+        //Called when the player wants to sleep
+        void StartSleep() {
+
+            //Set sleeping to true to let the while loop happen
+            IsSleeping = true;
+            Console.Clear();
+            Console.WriteLine("You have decided to sleep");
+
+            //Create a new random and get a random time to sleep
+            Random randomSleep = new Random();
+            int randomSleepTime = randomSleep.Next(3, 24 + 1);
+
+            //Create the timer for how long the while loop should run
+            SleepTimer = SetSleepTimer(randomSleepTime);
+            while (IsSleeping) {
+
+                Console.SetCursorPosition(0, 5);
+                for (int i = 0; i < 3; i++) {
+
+                    Console.Write(".");
+                    System.Threading.Thread.Sleep(200);
+
+                }
+
+            }
+
+            Console.Clear();
+            //Add the time slept to the game hours var
+            GameHours += randomSleepTime;
+            Console.WriteLine("You slept for " + randomSleepTime + " hours.");
+            Console.WriteLine("Press ENTER to continue");
+
+            Console.ReadKey();
+
+        }
+
+        //Creates a sleep timer
+        System.Timers.Timer SetSleepTimer(int time) {
+            //Create a timer
+            System.Timers.Timer aTimer = new System.Timers.Timer();
+            aTimer.Elapsed += new ElapsedEventHandler(SleepTimerElapsed);
+            aTimer.Interval = time * 500;
+            aTimer.Enabled = true;
+
+            return aTimer;
+        }
+
+        //Called when the sleep timer has elapsed
+        void SleepTimerElapsed(Object source, ElapsedEventArgs args) {
+            IsSleeping = false;
+            SleepTimer.Stop();
+            SleepTimer.Dispose();
+        }
+
+        //Sets up the attacks with the correct values
+        void SetAttacks() {
+
+        }
+    }
+}
